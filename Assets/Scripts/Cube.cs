a813892bdb510e8b48f8e07bd1f33f703345c697
rgweh <netonetonetot@gmail.com>
2025-03-01 13:54:55 +0700

    Remake
    made more classes

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,37 +7,29 @@ public class Cube : MonoBehaviour
     [SerializeField] private float _explodeRadius;
     [SerializeField] private float _explodeForse;
     [SerializeField] private ParticleSystem _effect;
-
     [SerializeField] private int _generation;
-    [SerializeField] private int _minAmount = 2;
-    [SerializeField] private int _maxAmount = 6;
 
     private float _scaleReduce = 2;
-    private float _actionChance;
-    private Renderer _cubeRenderer;
-    private GameObject _effectObject;
 
-    private float minXZ = -10f;
-    private float maxXZ = 10f;
-    private float minY = 0;
-    private float maxY = 10;
+    public event Action<GameObject> TryDuplicateCube;
+    public float ActionChance { get; private set; }
 
     private void OnEnable()
     {
         _generation++;
 
-        _actionChance = 100 / _generation;
+        ActionChance = 100 / _generation;
 
         Vector3 scale = transform.localScale;
         transform.localScale = new Vector3(scale.x / _scaleReduce, scale.y / _scaleReduce, scale.z / _scaleReduce);
 
-        _cubeRenderer = GetComponent<Renderer>();
-        _cubeRenderer.material.color = Random.ColorHSV();
+        var renderer = GetComponent<Renderer>();
+        renderer.material.color = UnityEngine.Random.ColorHSV();
     }
 
     private void OnMouseUpAsButton()
     {
-        TryDuplicate();
+        TryDuplicateCube?.Invoke(gameObject);
         Explode();
         Instantiate(_effect, transform.position, transform.rotation);
         Destroy(gameObject);
@@ -66,19 +58,4 @@ public class Cube : MonoBehaviour
         }
     }
 
-    private void TryDuplicate()
-    {
-        if(Random.Range(0, 100) <= _actionChance)
-        {
-            int amount = Random.Range(_minAmount, _maxAmount);
-
-            for (int i = 0; i < amount; i++)
-            {
-                Vector3 position = new Vector3(0f + Random.Range(minXZ,maxXZ), 0f + Random.Range(minY, maxY), 0f + Random.Range(minXZ, maxXZ));
-                Instantiate(gameObject, position, Quaternion.identity);
-            }
-        } 
-    }
 }
-
-
---
+++ Assets/Scripts/CubeDuplicator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDuplicator : MonoBehaviour
+{
+    [SerializeField] private Cube _cube;
+    [SerializeField] private int _minAmount = 2;
+    [SerializeField] private int _maxAmount = 6;
+
+    private float _rangeX;
+    private float _rangeY;
+    private float _rangeZ;
+
+    private void OnEnable()
+    {
+        _cube.TryDuplicateCube += OnTryDuplicateCube; 
+
+        _rangeX = transform.localScale.x/2;
+        _rangeY = transform.localScale.y/2;
+        _rangeZ = transform.localScale.z/2;
+    }
+
+    private void OnTryDuplicateCube(GameObject cubeObject)
+    {
+        Debug.Log("Trying to duplicate");
+        Cube parentCube = cubeObject.GetComponent<Cube>();
+
+        if (Random.Range(0, 100) <= parentCube.ActionChance)
+        {
+            int amount = Random.Range(_minAmount, _maxAmount);
+
+            for (int i = 0; i < amount; i++)
+            {
+                Vector3 position = GetRandomPosition();
+                GameObject newborn = Instantiate(cubeObject, position, Quaternion.identity);
+                Cube newbornCube = newborn.GetComponent<Cube>();
+                newbornCube.TryDuplicateCube += OnTryDuplicateCube;
+            }
+        }
+
+        Debug.Log("Tried succes!");
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        float x = Random.Range(transform.position.x - _rangeX, transform.position.x + _rangeX);
+        float y = Random.Range(transform.position.y - _rangeY, transform.position.y + _rangeY);
+        float z = Random.Range(transform.position.z - _rangeZ, transform.position.z + _rangeZ);
+
+        return new Vector3(x, y, z);
+    }
+}
